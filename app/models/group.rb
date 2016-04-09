64881b9e9adf1c710330bda93a1face9f9e92211
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2016-04-08 19:04:49 -0500

    full_name method defined in group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,4 +5,8 @@ class Group < ActiveRecord::Base
 
   validates :class_time, :coach_first_name, :coach_last_name, presence: true
 
+  def coach_full_name
+    "#{coach_first_name} #{coach_last_name}"
+  end
+
 end
