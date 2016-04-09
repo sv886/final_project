755af6048cbe9204cf7e73ce10be0c098bb989c4
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2016-04-08 19:46:41 -0500

    add member show

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,4 +4,8 @@ class Member < ActiveRecord::Base
 
   validates :member_first_name, :member_last_name, :age, :gender, presence: true
 
+  def member_full_name
+    "#{member_first_name} #{member_last_name}"
+  end
+
 end
