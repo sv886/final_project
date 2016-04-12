76391f03caab218a74ebf5958bdf98d6cb946bcf
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2016-04-11 23:05:37 -0500

    fade slide positioned above name

--- app/assets/javascripts/fadeCode.js
+++ app/assets/javascripts/fadeCode.js
@@ -1,9 +1,7 @@
 $(function (){
 
-    var yourFade = 1, // the amount of time in seconds that the elements will fade in AND fade out
-        yourDelay = 2, // the amount of time in seconds that there will be a delay between the fade ins and fade outs
-        fadeTime = yourFade * 1000, //convert fade seconds to milliseconds (1000)
-        delayTime = yourDelay * 1000, // convert delay seconds to milliseconds (2000)
+    var fadeTime = 750, // the amount of time in milliseconds that the elements will fade in AND fade out
+        delayTime = 2000, // // the amount of time in milliseconds that there will be a delay between the fade ins and fade outs
         totalTime = fadeTime + delayTime, //3000 milliseconds...needed for all those delays we talked about
         allElems, // find out exactly how many page elements have the 'toBeFaded' class (4)
         elemNoFade, // Will help us find the last element represent the last element (3)
@@ -17,7 +15,7 @@ $(function (){
         } else if (i === elemNoFade) {
             $(fadingElem).delay(totalTime * i).fadeIn(fadeTime);
         } else {
-            $(fadingElem).delay(totalTime * i).fadeIn(fadeTime).delay(delayTime).fadeOut(fadeTime);
+            ($(fadingElem).delay(totalTime * i).fadeIn(fadeTime).delay(delayTime).fadeOut(fadeTime));
         }
     }
 });
