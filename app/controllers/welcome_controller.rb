7b10be034f95850e18f18f78d35d95415837608d
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2016-04-13 15:48:42 -0500

    add navbar header partial w links, adjust width in view to 80%, add glyphicons in member show

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,11 @@
 class WelcomeController < ApplicationController
 
   before_action :authenticate_user!
-  
+  before_action do
+    @current_user = User.find_by id: session[:user_id]
+  end
+
   def index
+    redirect_to user_path(id: @current_user.id)
   end
 end
