a687ae596740557d11ca2641e68f397c69a3f177
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2016-04-13 16:11:46 -0500

    height method in meas model but also calculated in member show

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -5,4 +5,8 @@ class Measurement < ActiveRecord::Base
   validates  :weight, :bicep_circ, :forearm_circ, :chest_circ, :waist_circ, :abdomen_circ, :hips_circ, :thigh_circ, :calf_circ, :bmi, :body_fat_percentage, presence: true
   attachment :photo
 
+  def height_feet
+    "#{height_inches/12}' #{height_inches%12}\""
+  end
+
 end
