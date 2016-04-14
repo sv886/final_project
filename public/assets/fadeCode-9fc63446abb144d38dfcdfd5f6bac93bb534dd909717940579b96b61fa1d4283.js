7fb6ea2bb965743326a99e917fbea4a1a0575a24
Sebastian Villarreal <Sebastian@Sebastians-MacBook-Air.local>
2016-04-14 13:24:09 -0500

    local assets precompile

---
+++ public/assets/fadeCode-9fc63446abb144d38dfcdfd5f6bac93bb534dd909717940579b96b61fa1d4283.js
@@ -0,0 +1,21 @@
+$(function (){
+
+    var fadeTime = 1000, // the amount of time in milliseconds that the elements will fade in AND fade out
+        delayTime = 2000, // // the amount of time in milliseconds that there will be a delay between the fade ins and fade outs
+        totalTime = fadeTime + delayTime, //3000 milliseconds...needed for all those delays we talked about
+        allElems, // find out exactly how many page elements have the 'toBeFaded' class (x)
+        elemNoFade, // Will help us find the last element represent the last element (elem(x-1) bc start with elem0)
+        i,
+        fadingElem;
+
+    for (i = 0, allElems = $('.toBeFaded').length, elemNoFade = allElems - 1; i < allElems; i+=1) {
+        fadingElem = "#elem" + i;
+        if (i === 0) {
+            $(fadingElem).fadeIn(fadeTime).delay(delayTime).fadeOut(fadeTime);
+        } else if (i === elemNoFade) {
+            $(fadingElem).delay(totalTime * i).fadeIn(fadeTime);
+        } else {
+            ($(fadingElem).delay(totalTime * i).fadeIn(fadeTime).delay(delayTime).fadeOut(fadeTime));
+        }
+    }
+});
