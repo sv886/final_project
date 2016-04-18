cb50ae2658465663dd6fe69843ba44513ad7f68c
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2016-04-18 13:01:16 -0500

    edit github link on login, assets precompile hail mary for heroku

---
+++ public/assets/scaffold-9e4ef18975625a0f57edb3a9ceaf90998df398055fb7e47147969b1c67daaab2.js
@@ -0,0 +1,10 @@
+(function() {
+  $(function() {
+    return $('.chosen-select').chosen({
+      allow_single_deselect: true,
+      no_results_text: 'No results matched',
+      width: '200px'
+    });
+  });
+
+}).call(this);
