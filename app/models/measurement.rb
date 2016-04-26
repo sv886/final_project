9e59bf3e21356983a7cedd41c9362ddb8e09eb69
Sebastian Villarreal <Sebastian@Sebastians-MacBook-Air.local>
2016-04-26 18:17:10 -0500

    validation on measurement photo

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -2,7 +2,7 @@ class Measurement < ActiveRecord::Base
 
   belongs_to :member
 
-  validates  :member_id, :weight, :bicep_circ, :forearm_circ, :chest_circ, :waist_circ, :abdomen_circ, :hips_circ, :thigh_circ, :calf_circ, :bmi, :body_fat_percentage, presence: true
+  validates  :member_id, :photo, :weight, :bicep_circ, :forearm_circ, :chest_circ, :waist_circ, :abdomen_circ, :hips_circ, :thigh_circ, :calf_circ, :bmi, :body_fat_percentage, presence: true
   attachment :photo
 
   def height_feet
